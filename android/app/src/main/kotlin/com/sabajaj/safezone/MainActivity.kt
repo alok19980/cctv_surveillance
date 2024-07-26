2fbf6a0b396b4779546d0db9e3f9f626807c4a93
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2024-07-27 01:25:25 +0530

    my first commit

---
+++ android/app/src/main/kotlin/com/sabajaj/safezone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sabajaj.safezone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shop/route/route_constants.dart';
+import 'package:shop/route/router.dart' as router;
+import 'package:shop/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shop Template by The Flutter Way',
+      theme: AppTheme.lightTheme(context),
+      
+      themeMode: ThemeMode.light,
+      onGenerateRoute: router.generateRoute,
+      initialRoute: onbordingScreenRoute,
+    );
+  }
+}
